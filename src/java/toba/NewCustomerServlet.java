6f696e214728241673371079269ce0f45bb699c3
Matt <Matt@Conky-2000>
2016-03-29 01:44:59 -0400

    Assignment 1

---
+++ src/java/toba/NewCustomerServlet.java
@@ -0,0 +1,100 @@
+package toba;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Matt
+ */
+@WebServlet(urlPatterns = {"/NewCustomerServlet"})
+public class NewCustomerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String Firstname = request.getParameter("Firstname");
+            String Lastname = request.getParameter("Lastname");
+            String Phone = request.getParameter("Phone");
+            String Address = request.getParameter("Address");
+            String City = request.getParameter("City");
+            String Zipcode = request.getParameter("Zipcode");
+            String Email = request.getParameter("Email");
+            
+            if(Firstname.equals("")||Lastname.equals("")||Phone.equals("")||Address.equals("")||City.equals("")||Zipcode.equals("")||Email.equals(""))
+            {
+                out.println("<script type=\"text/javascript\">");  
+                out.println("alert('Please fill out all of the Fields in order to Create your Account!');");
+                out.println("window.location=\'New_customer.html';");
+                out.println("</script>");
+            }
+            else
+            {
+                response.setStatus(response.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", "Success.html");  
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
